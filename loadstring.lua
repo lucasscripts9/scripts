d4befbab3afaf6359de00f5bce2d33e593c0b007
lucasscripts9 <rodrigueslegallucas@gmail.com>
2026-02-25 23:18:14 -0400

    Atualizar o loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,16 +1,9 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/mobilitylordv1.lua')))()
 
-local args = {
-	"shadow"
-}
-game:GetService("ReplicatedStorage"):WaitForChild("remoteEvents"):WaitForChild("characterRequest"):FireServer(unpack(args))
-
-task.wait(2)
+task.wait(3)
 
 local player = game.Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
-
-task.wait(3)
 
 local dash = playerGui:FindFirstChild("gui")
 	and playerGui.gui:FindFirstChild("ingameHud")
