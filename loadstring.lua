37930facd0923b4ad243c78afa9a51e3afbb447a
lucasscripts9 <rodrigueslegallucas@gmail.com>
2026-02-25 23:15:17 -0400

    Atualizar o loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,10 +1,23 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/mobilitylordv1.lua')))()
+
 local args = {
 	"shadow"
 }
 game:GetService("ReplicatedStorage"):WaitForChild("remoteEvents"):WaitForChild("characterRequest"):FireServer(unpack(args))
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/mobilitylordv1.lua')))()
-
 task.wait(2)
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/deletething.lua')))()
+local player = game.Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+
+task.wait(3)
+
+local dash = playerGui:FindFirstChild("gui")
+	and playerGui.gui:FindFirstChild("ingameHud")
+	and playerGui.gui.ingameHud:FindFirstChild("abilities")
+	and playerGui.gui.ingameHud.Abilities:FindFirstChild("mobile")
+	and playerGui.gui.ingameHud.Abilities.mobile:FindFirstChild("Dash")
+
+if dash then
+	dash:Destroy()
+end
