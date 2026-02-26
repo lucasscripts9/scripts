2641f476d16886fde4db5bccf1379deddebdb85e
lucasscripts9 <rodrigueslegallucas@gmail.com>
2026-02-25 22:54:41 -0400

    Criar loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,9 @@
+local args = {
+	"shadow"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("remoteEvents"):WaitForChild("characterRequest"):FireServer(unpack(args))
+
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/deletething.lua')))()
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/mobilitylordv1.lua')))()
