b1385c4d185a8c3f374fd4613187fb771c8c2953
lucasscripts9 <rodrigueslegallucas@gmail.com>
2026-02-25 23:09:39 -0400

    Atualizar o loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,7 +3,8 @@ local args = {
 }
 game:GetService("ReplicatedStorage"):WaitForChild("remoteEvents"):WaitForChild("characterRequest"):FireServer(unpack(args))
 
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/mobilitylordv1.lua')))()
+
+task.wait(2)
 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/deletething.lua')))()
-
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasscripts9/scripts/refs/heads/main/mobilitylordv1.lua')))()
